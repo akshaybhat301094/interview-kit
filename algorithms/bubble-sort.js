bfe65ad983126a2bc1e9aa3c05e6395709bf824c
arjunbaru <akshaybhat301094@gmail.com>
2021-04-02 10:29:46 +0530

    added description in bubble sort

--- algorithms/bubble-sort.js
+++ algorithms/bubble-sort.js
@@ -1,3 +1,16 @@
+/**
+ * In bubble sort we have to find the largest element and push it to the end of the arr;
+ * We compare each element with its next and if the next is smaller we swap
+ * Also after each pass as we know the largest item will be at the end we check till one less than end
+ *
+ *      [5,2,6,8,3,1]
+ *      [2,5,6,8,3,1]
+ *      [2,5,6,3,8,1]
+ *      [2,5,6,3,1,8] --- first pass completed
+ *
+ *
+ */
+
 function bubbleSort(arr) {
   let noSwaps;
   let swap = (arr, item1, item2) => {
