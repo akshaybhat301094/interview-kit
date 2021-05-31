ec84cf6247c8c1d808c4541bf84f07fc7232ca6d
arjunbaru <akshaybhat301094@gmail.com>
2021-05-31 16:27:42 +0530

    added new js tricks

--- javascript-tricks.js
+++ javascript-tricks.js
@@ -19,11 +19,6 @@ let arr = [1, 2, 3];
 
 arr.map(String); // -> will return ["1", "2", "3"]
 
-// Alternate of using Math.floor
-let a = ~~10.4; // -> will return 10
-// one issue with this approach though for negative numbers it will not round of the value
-// eg ~~(-5.5) will return -5 but Math.floor(-5.5) returns -6
-
 // param validataion example
 const isRequired = () => {
   throw new Error('param is required');
@@ -35,3 +30,51 @@ const print = (num = isRequired()) => {
 
 print(2); // outpup: printing 2
 print(); // output : Error: param is required
+
+// alternate of switch case with object literals.
+
+const handleSaveCalculation = ({ key }) => {
+  const actions = {
+    'save-copy': saveCopy,
+    override: override,
+    default: () => {
+      throw Error('Unknown action');
+    },
+  };
+
+  const action = key in actions ? actions[key] : actions['default'];
+  return action();
+};
+
+// bitwise
+
+// Alternate of using Math.floor
+let a = ~~10.4; // -> will return 10
+// one issue with this approach though for negative numbers it will not round off the value
+// eg ~~(-5.5) will return -5 but Math.floor(-5.5) returns -6
+
+let arr = [1, 2, 3, 4, 5];
+let idx = arr.indexOf(6);
+
+// Traditional
+if (idx !== -1) {
+  // Do something
+}
+
+// Alternate
+if (Boolean(~idx)) {
+  // Do Something
+}
+
+// Another alternate
+if (Boolean(idx ^ -1)) {
+  // Do something
+}
+
+// even or odd
+
+let num = 34;
+console.log(num & 1); // returns 0 -> even
+
+let num = 35;
+console.log(num & 1); // returns 1 -> odd
