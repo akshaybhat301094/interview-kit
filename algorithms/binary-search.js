75d41059f7e85ab40604ebb94e0e4d2e3d945c7a
AKSHAY BHAT <akshaybhat301094@gmail.com>
2022-03-26 12:06:19 +0530

    changes to while loop
    the previous logic can break in the following case:-
    
    arr = [-1,0,3,5,9,12];
    val = 2;
    
    since we were checking for the mid element in while condition before it would make it run infinitely.

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -17,20 +17,20 @@
   function binarySearch(arr, val) {
     let start = 0;
     let end = arr.length - 1;
-    let mid = ~~((start + end) / 2);
+    
 
     if (val > arr[end] || val < arr[start]) {
       return -1;
     }
 
-    while (arr[mid] !== val) {
+    while (start <= end) {
+      let mid = ~~((start + end) / 2);
+      if(arr[mid] === val) return mid;
       if (val < arr[mid]) end = mid - 1;
       else start = mid + 1;
-
-      mid = ~~((start + end) / 2);
     }
 
-    return mid;
+    return -1;
   }
 
   global.binarySearch = binarySearch;
